6a9d28a7ff16760fbf431e8da0b5977a0bd0ad34
GabrielBeneli <63161814+GabrielBeneli@users.noreply.github.com>
2020-06-02 16:55:33 -0300

    Fim testes hsv

--- codigo do beneli/Analisequalidadeopencv/main.py
+++ codigo do beneli/Analisequalidadeopencv/main.py
@@ -14,9 +14,9 @@ for filename in os.listdir(directory):
 
         res, img = tm.find(partPath)
 
-        plt.subplot(121),plt.imshow(res,cmap = 'grey')
+        plt.subplot(121),plt.imshow(res,cmap = 'gray')
         plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-        plt.subplot(122),plt.imshow(img,cmap = 'grey')
+        plt.subplot(122),plt.imshow(img,cmap = 'gray')
         plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
 
         plt.show( )
